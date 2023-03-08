26ab709189327bb0a45569d796c2b603f2bc4578
M Angeles Cordoba <mangeles.cg91@gmail.com>
2023-03-08 21:35:42 +0100

    Evitar campos reperidos en la BBDD

--- bdd_miniproyecto.sql
+++ bdd_miniproyecto.sql
@@ -4,10 +4,10 @@ use miniproyecto;
 
 create table usuario(
 	id_usuario int auto_increment primary key,
-    nombre varchar(255),
-    email varchar(255),
-    pass varchar(255),
-    tipo_usuario int
+    nombre varchar(255) not null,
+    email varchar(255) unique not null,
+    pass varchar(255) not null,
+    tipo_usuario int not null
 );
     
 insert into usuario(nombre, email, pass, tipo_usuario) values('admin1', 'adm1@adm.com', '1234', 0);
