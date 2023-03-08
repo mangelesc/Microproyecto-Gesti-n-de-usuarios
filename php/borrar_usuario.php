88704f8feb23b9cb5cccc7dc6d9e03ee177d1983
M Angeles Cordoba <mangeles.cg91@gmail.com>
2023-03-08 21:19:31 +0100

    control errores borrar usuario

--- php/borrar_usuario.php
+++ php/borrar_usuario.php
@@ -7,11 +7,22 @@ adminControl();
 
 require_once ("../database/database.php");
 
-if(isset($_GET['id_usuario']))
-      {
-        $id_usuario=$_GET['id_usuario'];
-        borrarUsuario($id_usuario);
-        header("Location: ./admin.php");
+if(isset($_GET['id_usuario'])){
+    if($_SESSION['id_usuario']==$_GET['id_usuario']){
+        echo "<script type='text/javascript'>alert('Oops, no te puedes borrar a tí mismo!');</script>";
+        header("Refresh:0; url=./admin.php");
+    }else{
+      $id_usuario=$_GET['id_usuario'];
+      try {
+          borrarUsuario($id_usuario);
+          echo "<script type='text/javascript'>alert('Usuario borrado correctamente');</script>";
+      } catch (Exception $e){
+          echo "<script type='text/javascript'>alert('Ha habido un error, el usuario no ha podido ser borrado');</script>";
       }
+      header("Refresh:0; url=./admin.php");
+      
+    }
+    
+  }
 
 ?>
